990366f8394a4309dedbf9bf0f39c094034ae2be
InitTester <143479869+InitTester@users.noreply.github.com>
2023-09-12 08:59:22 +0900

    Add files via upload

---
+++ ch02/src/Ex2_11.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Ex2_11 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub		
+		short sMin = -32768, sMax = 32767;
+		char  cMin = 0, 	 cMax = 65535;
+		
+		System.out.println("sMin   = "+ sMin);
+		System.out.println("sMin-1 = "+ (short)(sMin-1));
+		System.out.println("sMax = "+ sMax);
+		System.out.println("sMax+1 = "+ (short)(sMin+1));
+		System.out.println("cMin = "+ (int)cMin);
+		System.out.println("cMin-1 = "+ (int)--cMin);
+		System.out.println("cMax = "+ (int)cMax);
+		System.out.println("cMax+1 = "+ (int)++cMax);
+		// 최고값에서 더하면 최소값, 최소값에서 빼면 최대값 오버플로우
+	}
+}
