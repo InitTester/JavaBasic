290ac11d4744c37d003437a9cd533e486acbe9c7
InitTester <143479869+InitTester@users.noreply.github.com>
2023-09-13 23:58:56 +0900

    Add files via upload

---
+++ ch04/src/Ex4_2.java
@@ -0,0 +1,21 @@
+
+public class Ex4_2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x = 0;
+		System.out.printf("x=%d 일때, 참인것은%n",x);
+		
+		if(x==0) System.out.println("x==0");
+		if(x!=0) System.out.println("x!=0");
+		if(!(x==0)) System.out.println("!(x==0)");
+		if(!(x!=0)) System.out.println("!(x!=0)");
+		
+		x =1;
+		
+		if(x==0) System.out.println("x==0");
+		if(x!=0) System.out.println("x!=0");
+		if(!(x==0)) System.out.println("!(x==0)");
+		if(!(x!=0)) System.out.println("!(x!=0))");
+	}
+}
